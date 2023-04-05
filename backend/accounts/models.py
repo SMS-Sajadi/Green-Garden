33ba86032bbe4502f2f79a6bb7365f81f82d5367
SMS-Sajadi <soroush.s2017@gmail.com>
2023-04-05 15:09:57 +0330

    Making a simple User model :\

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class UserBase(AbstractUser):
+    email = models.EmailField(max_length=200, unique=True)
+    username = models.CharField(max_length=50)
+    city = models.CharField(max_length=50, null=True)
+    profile_photo = models.ImageField()
+    phone_number = models.IntegerField(null=True, blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
