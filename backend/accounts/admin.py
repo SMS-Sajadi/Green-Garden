ca9077e7e0ee830041a0ccedc40f37a8ee46138e
mohammad <9m.kh557@gmai.com>
2023-04-06 00:35:36 +0330

    login

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserBase
+from .models import User
 
-admin.site.register(UserBase)
+admin.site.register(User)
 
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import User
 
 # Create your views here.
+
+
+def login_user(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['pass']
+
+        user = User.objects.filter(email=email, password=password)
+        if not user:
+            return HttpResponse("test sign up page")
+
+        user = user[0]
+        login(request, user)
+        return HttpResponse("test home page")
