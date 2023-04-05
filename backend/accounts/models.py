b1bc11aa158dbbade1e06f146b04c3a34fa87a65
mohammad <9m.kh557@gmai.com>
2023-04-05 20:26:38 +0330

    remove username

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -4,11 +4,10 @@ from django.contrib.auth.models import AbstractUser
 
 class UserBase(AbstractUser):
     email = models.EmailField(max_length=200, unique=True)
-    username = models.CharField(max_length=50)
     city = models.CharField(max_length=50, null=True)
     profile_photo = models.ImageField()
     phone_number = models.IntegerField(null=True, blank=True)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username"]
+    REQUIRED_FIELDS = []
 
